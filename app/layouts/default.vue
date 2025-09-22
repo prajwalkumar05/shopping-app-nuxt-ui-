ce813a8ae8814e73c085b68532c15adf949a9252
Prajwal Kumar <85426811+prajwalkumar05@users.noreply.github.com>
2025-09-22 16:17:29 +0530

    update the version to 0.0.2

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="h-screen flex flex-col overflow-hidden">
     <Navbar />
     <main class="bg-[#F4F2EE] flex-1  overflow-hidden">
-      <p>v{{ config.public.APP_VERSION }}</p>
+      <p text-green>v{{ config.public.APP_VERSION }}</p>
       <slot />
     </main>
     <UNotifications />
