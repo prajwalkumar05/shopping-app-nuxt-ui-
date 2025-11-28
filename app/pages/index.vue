cc3177a48065b2371b3ea5b24b4c0bc302ae94e7
Prajwal Kumar <85426811+prajwalkumar05@users.noreply.github.com>
2025-11-28 10:47:52 +0530

    change homepage text

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
   <div class="h-full flex flex-col">
     <div class="flex justify-center bg-primary">
       <div class="w-full max-w-[1200px] px-4 ">
-        <h1 class="text-2xl text-primary font-bold">Products list</h1>
+        <h1 class="text-2xl text-primary font-bold">Products</h1>
       </div>
     </div>
 
